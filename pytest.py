710b58f4f1aed3635763c27d6a6467ed73fd6cf1
kohalhoq <64858485+kohalhoq@users.noreply.github.com>
2020-12-15 16:04:51 -0500

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,1 +1,14 @@
 import pytest
+from person import travelling_overseas
+
+def test_person(time, location):
+  assert person_travelling_overseas(16, Kenya) = Level 3 risk area
+  assert person_travelling_overseas( 25, Vatican city) = Level 1 risk area
+  assert person_travelling_overseas (10, Brazil) = Level 4 risk area
+
+def test_person_edge(time,location):
+ assert person_travelling_overseas(27, Tunisia) = Level 2 risk area
+ assert person_travelling_overseas( 45, Venezuela) = Level 3 risk area
+ assert person_travelling_overseas (32, Poland) = Level 4 risk area
+  
+  
