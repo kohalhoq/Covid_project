581e6fc14e956dd77b8c8a1b5778300ccf798c24
kohalhoq <64858485+kohalhoq@users.noreply.github.com>
2020-12-15 13:09:41 -0500

    Update person.py

--- person.py
+++ person.py
@@ -16,6 +16,7 @@ class Person:
         Current country of residence for the person
     is_smoker : boolean
         A boolean representing whether the person smokes or not.
+     """
 
     
 
@@ -25,31 +26,7 @@ class Person:
         self.age = age
         self.country = country
         self.is_smoker = is_smoker
-        
-        
-def answer_questions():
-    """Asks questions to get some personal information about the person and creates an instance of the Person class."""
-    name = input("Please enter your full name : ")
-    print(f"Hi {name}! Hope you're doing well. Kindly take some time out to answer this questionnaire.")
-    print("")
-    age = input("What's your age? (Only enter a positive integer value)")
-    try:
-        age = int(age)
-    except Exception as e:
-        print("Please enter a valid value for age (positive integer)")
-        age = input("What's your age? (Only enter a positive integer value)")
-        age = int(age)
-    country = input("What is your country of residence?")
-    is_smoker = input("Are you a smoker? (Y/N)")
-    if 'y' in is_smoker.lower():
-        is_smoker = True
-    else:
-        is_smoker = False
-    
-    return Person(name, age, country, is_smoker)
-
-
-def questions(list_of_symptoms,symptoms_span, in_or_out ):
+        def questions(list_of_symptoms,symptoms_span, in_or_out ):
     """ This function will ask the questions about list_of_symptoms for covid
     Attributes : 
     list_of_symptoms: will consist of questions about list_of_symptoms the users are encountering 
@@ -91,12 +68,8 @@ def questions(list_of_symptoms,symptoms_span, in_or_out ):
     else:
         print ("Not valid")
         
-questions('list_of_symptoms','symptoms_span','in_or_out')
-
-
-
-        
-def state_statistics(self):
+    questions('list_of_symptoms','symptoms_span','in_or_out')
+    def state_statistics(self):
         """Method to request a users state and supply the current covid statistics for the given state
 
         Returns:
@@ -114,7 +87,7 @@ def state_statistics(self):
                 for item in range(len(row.split(","))):
                     print(columns[item].replace("\n","")+": "+row.split(",")[item])
                     
-def testing_centers(self):
+    def testing_centers(self):
         """Method to request a users location and return nearby covid testing centers
 
         Returns:
@@ -129,7 +102,7 @@ def testing_centers(self):
                 print(f"\n----------\nName: {line.split(',')[0]}\nState: {line.split(',')[1]}\nCity: {line.split(',')[2]}")
       
       
-def travelling_overseas(self, time, location, frequency):
+    def travelling_overseas(time, location, frequency):
     """ This function will ask the question about travelling informations of the users 
     Attributes: 
     time: time spend in any foreign location
@@ -138,10 +111,9 @@ def travelling_overseas(self, time, location, frequency):
     Returns: 
     travelling_statistics: take the provided statistics and identify the risk of being exposed to covid
     """
-    self.time= time
+    
     time = input("Amount of time spent on the location in days")
     print(time)
-    self.location = location
     location = input("Enter the location you have travelled to: ")
     print(location)
     list = df['A'].tolist(loc)
@@ -159,20 +131,39 @@ def travelling_overseas(self, time, location, frequency):
         return f("Level 2 risk area")
     else:
         return f("Level 1 risk area")
-        
-        
-    self.frequency = frequency 
     frequency = input("What is the frequency of your travel:")
     if frequency > 1:
         print (" Please retake the quiz if you have travelled more than once using different location " )
     else:
         return frequency 
+        
+def answer_questions():
+    """Asks questions to get some personal information about the person and creates an instance of the Person class."""
+    name = input("Please enter your full name : ")
+    print(f"Hi {name}! Hope you're doing well. Kindly take some time out to answer this questionnaire.")
+    print("")
+    age = input("What's your age? (Only enter a positive integer value)")
+    try:
+        age = int(age)
+    except Exception as e:
+        print("Please enter a valid value for age (positive integer)")
+        age = input("What's your age? (Only enter a positive integer value)")
+        age = int(age)
+    country = input("What is your country of residence?")
+    is_smoker = input("Are you a smoker? (Y/N)")
+    if 'y' in is_smoker.lower():
+        is_smoker = True
+    else:
+        is_smoker = False
+    
+    return Person(name, age, country, is_smoker)
 
 
-def main1(symptoms,symptoms_span,in_or_out):
+
+
+
+def main1(symptoms,symptoms_span,in_or_out,time, location, frequency):
     for symptoms, symptoms_span, in_or_out in questions(symptoms, symptoms_span, in_or_out):
         print(symptoms, symptoms_span, in_or_out)
-
-def main2(time, location, frequency):
     for time, location, frequency in travelling_overseas(time, location, frequency):
         print(time, location, frequency)
