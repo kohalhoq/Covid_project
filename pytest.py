56fb99a0aaf2b7992fff3cf1db69ca9922ba646d
Cmclark21 <67977125+Cmclark21@users.noreply.github.com>
2020-12-18 20:57:53 -0500

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,5 +1,7 @@
 import pytest
 from person import travelling_overseas
+from second import Person
+import hashlib
 
 def test_questions():
     with mock.patch("builtins.input", 
@@ -26,3 +28,13 @@ def test_use_gender():
   person2 = Person('James', 20, 'USA', False' 'M')
   assert person1.use_gender() = 'Women are less likely to die from COVID-19. Still be careful though!'
   assert person2.use_gender() = 'Men have a 20% higher chance of dying from COVID-19. Stay safe!'
+                   
+                   
+
+def test():
+    p = Person()
+    assert str(hashlib.sha1(p.testing_centers("PA", "LANCASTER").encode()).hexdigest()) == "18f511a2f62cd66cb0927c7f16bbe4c7d7aeb5a6"
+    assert str(hashlib.sha1(p.testing_centers("WY", "WORLAND").encode()).hexdigest()) == "95dad92de06cb4cef6a5ba8cbd71a617c2ce423c"
+    assert str(hashlib.sha1(p.testing_centers("AK", "ANCHOR POINT").encode()).hexdigest()) == "db6729e9d51812cbea8dce730ce7073d6a2e980b"
+    assert str(hashlib.sha1(p.testing_centers("CA", "SAN DIEGO").encode()).hexdigest()) == "42d4c25c6417f82b406505fbbb49b82c79ad369a"
+test()
